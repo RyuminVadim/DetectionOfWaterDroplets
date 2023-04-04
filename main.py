b35bad73d1c7c0e5bd2d68f08166e41886fda591
V4SYA <62791870+V4SYA@users.noreply.github.com>
2023-04-04 20:02:09 +0700

    The code changing

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 import cv2
-import matplotlib.pyplot as plt
+from sympy import Point, Ellipse
+import numpy as np
 
-image = cv2.imread("ex_1.jpg")
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+x1 = 'ex_2.jpg'
 
-_, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-plt.imshow(binary, cmap = "gray")
-plt.show()
+image = cv2.imread(x1, 0)
+image1 = cv2.imread(x1, 1)
 
-contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-image = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
-plt.imshow(image)
-plt.show()
-cv2.imwrite("result.jpg", image)
+x, y = image.shape
+
+median = cv2.GaussianBlur(image, (19, 19), 0)
+median1 = cv2.GaussianBlur(image, (21, 21), 0)
+
+a = median1-median
+
+c = 255-a
+
+ret, thresh1 = cv2.threshold(c, 200, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
+
+kernel = np.ones((4, 4), np.uint8)
+
+dilation = cv2.dilate(thresh1, kernel, iterations = 1)
+
+kernel = np.ones((6, 6) , np.uint8)
+
+opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
+
+cv2.imwrite('test_2.jpg', opening)
+
+contours, hierarchy = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+c = np.size(contours[:])
+
+Blank_window = np.zeros([x, y, 3])
+Blank_window = np.uint8(Blank_window)
+
+for u in range(0, c-1):
+    if (np.size(contours[u]) > 20):
+        ellipse = cv2.fitEllipse(contours[u])
+        (center, axes, orientation) = ellipse
+        majoraxis_length = max(axes)
+        minoraxis_length = min(axes)
+        eccentricity = (np.sqrt(1-(minoraxis_length/majoraxis_length)**2))
+        if (eccentricity < 0.8):
+             cv2.drawContours(image1, contours, u, (255, 1, 255), 3)
+
+cv2.imwrite('marked_2.jpg', image1)
